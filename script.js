4c2efd8bc5765115bdffa55aa95ca8a3b6003d8c
Júlio César <juliocesaroalves679@gmail.com>
2025-04-22 20:57:24 -0300

    Atualizações nas importações

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-import {criarPartida, lista_ID_partidas, lista_partidas} from "./Funções/funções_partida.js";
-import {criarJogador, lista_ID_jogadores, lista_jogadores} from "./Funções/funções_jogador.js"
+import {criarPartida, lista_ID_partidas, lista_partidas} from "./Funções/funcoes_partida.js";
+import {criarJogador, lista_ID_jogadores, lista_jogadores} from "./Funções/funcoes_jogador.js";
+
 
 
 //Funções de exibição de texto
