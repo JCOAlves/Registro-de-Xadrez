03be01d475c71fdb8f52e4cda9b5e276e2f45e42
Júlio César <juliocesaroalves679@gmail.com>
2025-04-27 15:07:47 -0300

    Criação de funções de validação de campo de cadastro de jogadores

--- Scripts/script_partida.js
+++ Scripts/script_partida.js
@@ -1,1 +1,2 @@
 import {criaPartida, lista_partidas} from "../Funções/funcoes_partida.js";
+import {criaJogada} from "../Funções/funcoes_jogada.js";
--- Scripts/scripts_cadastro.js
+++ Scripts/scripts_cadastro.js
@@ -1,16 +1,47 @@
-import {criaJogador, lista_jogadores} from "../Funções/funcoes_jogador.js";
-import {criaJogada} from "../Funções/funcoes_jogada.js";
+import {criaJogador} from "../Funções/funcoes_jogador.js";
 
+//Funções de validação dos campos
+const validaNome = () => {
+    const nome = document.getElementById('nome').value;
+    if(nome.length < 10){
+        document.getElementById('nome').style.borderColor = 'red';
+        alert('Nome invalido.');
+    }
+}
 
+const validaNomeUsuario = () => {
+    const nomeUsuario = document.getElementById('nomeUsuario').value;
+    if(nomeUsuario.length < 8 || nomeUsuario.length > 10 || nomeUsuario.includes(' ')){
+        document.getElementById('nomeUsuario').style.borderColor = 'red';
+        alert('Nome de Usuario invalido.');
+    }
+}
 
+/*const validaNascimento = () =>{
+    const data = new Date(document.getElementById('nascimento').value);
+    let dia = Number(data.getUTCDate());
+    if(dia < 10){
+        dia = "0"+dia
+    }
 
-//Funções de exibição de texto
-const exibirTexto = (id, texto) => {
-    document.getElementById(id).innerHTML = texto;
-};
+    let mes = Number(data.getUTCMonth())+1;
+    if(mes < 10){
+        mes = "0"+mes
+    }
 
-const cadastroJogador = () => {
-    
+    let ano = data.getUTCFullYear();
+
+    const nascimento = `${dia}/${mes}/${ano}`;
+    alert(nascimento);
+}*/
+
+const cadastroJogador = (event) => {
+    event.preventDefault();
+    const nome = document.getElementById('nome').value;
+    const nomeUsuario = document.getElementById('nomeUsuario').value;
+    const nascimento = new Date(document.getElementById('nascimento').value);
+    const genero = document.getElementById('genero').value;
+    criaJogador()
 }
 
 
