26a35c20ce84c7fe2d9924325409d2b91b7b2e27
Júlio César <juliocesaroalves679@gmail.com>
2026-04-15 20:59:09 +0000

    feat(Backend): Creation of RespostaHTTP class

--- Backend/Controllers/controllerUsuarios.js
+++ Backend/Controllers/controllerUsuarios.js
@@ -1,17 +1,24 @@
 import Jogador from "../Models/Jogador.js";
 import Usuario from "../Models/Usuario.js";
+import RespostaHTTP from "../Models/RespostaHTTP.js";
 
 const listaUsuarios = async (req, res) => {
     try {
         const listaUsuarios = await Usuario.findAll();
         if(listaUsuarios.length > 0){
-            res.status(404).json()
+            const Resposta = new RespostaHTTP(true, "Usuários listados com sucesso", null, listaUsuarios);
+            Resposta.ExibiMensagem();
+            res.status(200).json(Resposta.RetornaResposta('returnListDados'));
         } else{
-            res.status(404).json()
+            const Resposta = new RespostaHTTP(false, "Não há usuários cadastrados no sistema", "Não há usuários cadastrados no sistema");
+            Resposta.ExibiMensagem();
+            res.status(404).json(Resposta.RetornaResposta());
         }
         
     } catch (error) {
-        res.status(500).json()
+        const Resposta = new RespostaHTTP(false, "Erro na listagem de usuários no sistema", error.message | error);
+        Resposta.ExibiMensagem('Erro');
+        res.status(500).json(Resposta.RetornaResposta());
     }
 };
 
@@ -19,36 +26,45 @@ const lista_tipoUsuarios = async (req, res) => {
     try {
         const { tipoUsuario } = req.params;
         if(!["Jogador", "Administrador"].includes(tipoUsuario)){
-            
-        } 
+            const Resposta = new RespostaHTTP(false, "Tipo de usuário fornecido invalido", "Tipo de usuário fornecido invalido");
+            Resposta.ExibiMensagem();
+            res.status(400).json(Resposta.RetornaResposta());
+        }
         
-        let listaUsuario = [];
+        let parametrosConsulta = {};
         switch(tipoUsuario){
             case "Jogador":
-                listaUsuario = await Usuario.findAll({
+                parametrosConsulta = {
                     where: { tipoUsuario: "Jogador" },
                     include: {
                         model: Jogador,
                         required: true, // Força o INNER JOIN
                         attributes: ['ID_jogador', 'nicknameJogador']
                     }
-                });
+                };
                 break;
             case "Administrador":
-                listaUsuario = await Usuario.findAll({
+                parametrosConsulta = {
                     where: { tipoUsuario: "Administrador" }
-                });
+                };
                 break;
         }
 
+        const listaUsuario = await Usuario.findAll(parametrosConsulta);
         if(listaUsuario.length > 0){
-
+            const Resposta = new RespostaHTTP(true, "Usuários listados por tipo feito com sucesso", null, listaUsuario);
+            Resposta.ExibiMensagem();
+            res.status(200).json(Resposta.RetornaResposta('returnListDados'));
         } else{
-
+            const Resposta = new RespostaHTTP(false, "Não há usuários desse tipo cadastrados no sistema", "Não há usuários desse tipo cadastrados no sistema");
+            Resposta.ExibiMensagem();
+            res.status(404).json(Resposta.RetornaResposta());
         }
 
     } catch (error) {
-        
+        const Resposta = new RespostaHTTP(false, "Erro na listagem de usuários por tipo", error.message | error);
+        Resposta.ExibiMensagem('Erro');
+        res.status(500).json(Resposta.RetornaResposta());
     }
 };
 
--- Backend/Models/RespostaHTTP.js
+++ Backend/Models/RespostaHTTP.js
@@ -1,11 +1,52 @@
 // Classe de resposta de requisições HTTP
 
-class Resposta {
-    constructor(sucesso, mensagem, erro){
-        this.sucesso = sucesso ? true : false;
+class RespostaHTTP {
+    constructor(sucesso=false, mensagem, erro, dados, quantidade=1){
+        this.sucesso = sucesso;
         this.mensagem = mensagem;
-        this.erro = erro ? erro : null;
+        this.erro = erro;
+        this.dados = dados;
+        this.quantidade = dados.length ? dados.length : quantidade;
     }
+
+    RetornaResposta(tipoResposta=""){
+        switch(tipoResposta){
+            case "returnListDados":
+                return {
+                    sucesso: this.sucesso, 
+                    mensagem: this.mensagem,
+                    quantidade: this.quantidade,
+                    dados: this.dados, 
+                    erro: this.erro
+                };
+                
+            case "returnDado":
+                return {
+                    sucesso: this.sucesso, 
+                    mensagem: this.mensagem,
+                    dados: this.dados,
+                    erro: this.erro
+                };
+
+            default:
+                return {
+                    sucesso: this.sucesso, 
+                    mensagem: this.mensagem,
+                    erro: this.erro
+                };
+        };
+    };
+
+    ExibiMensagem(tipoMensagem=""){
+        switch(tipoMensagem){
+            case "Erro":
+                console.log(`${this.mensagem}: ${this.erro}`);
+                break;
+            default:
+                console.log(this.mensagem);
+        }
+    }
+
 };
 
-export default Resposta;
+export default RespostaHTTP;
