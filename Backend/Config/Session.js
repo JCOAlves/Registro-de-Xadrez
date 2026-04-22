653c354d71398e491ab76b80136def663082cb04
Júlio César <juliocesaroalves679@gmail.com>
2026-04-21 21:32:31 -0300

    feat(Backend): Adição de funcionalidade login com session

---
+++ Backend/Config/Session.js
@@ -0,0 +1,13 @@
+import session from "express-session";
+
+const ConfigSession = {
+    name: 'registerXadrez', // Nome do cookie
+    secret: 'breut5y498t754ugh857t94308tru354yht9348trerh984r329rt4', // Essencial para assinar o cookie. Substitua por uma string segura
+    resave: true, // Salva a sessão mesmo se não modificada
+    saveUninitialized: true, // Salva sessão para usuários não logados
+    cookie: { secure: true } // Em produção, use secure: true e HTTPOnly. Defina como true se usar HTTPS
+};
+
+session(ConfigSession);
+
+export default session;
--- Backend/Controllers/controllerLogin.js
+++ Backend/Controllers/controllerLogin.js
@@ -1,13 +1,55 @@
 import Usuario from "../Models/Usuario.js";
 import Jogador from "../Models/Jogador.js";
+import session from "express-session";
+import RespostaHTTP from "../Models/RespostaHTTP.js";
 
 // Script com funções de Login, confirmLogin e Logout
 
 const Login = async (req, res) => {
     try {
+        const { emailUsuario, senhaUsuario } = req.body;
+
+        if(!emailUsuario){
+            const Resposta = new RespostaHTTP(false, "Email de usuário não foi fornecido para o login", "Email de usuário não foi fornecido para o login");
+            Resposta.ExibiMensagem();
+            return res.status(400).json(Resposta.RetornaResposta());
+        };
+
+        if(!senhaUsuario){
+            const Resposta = new RespostaHTTP(false, "Senha de usuário não foi fornecido para o login", "Senha de usuário não foi fornecido para o login");
+            Resposta.ExibiMensagem();
+            return res.status(400).json(Resposta.RetornaResposta());
+        };
+
+        let loginUsuario = await Usuario.findOne({ 
+            where: { emailUsuario: emailUsuario, senhaUsuario: senhaUsuario }, 
+            attributes: ['ID_usuario', 'nomeUsuario', 'emailUsuario', 'tipoUsuario'] 
+        });
+        if(loginUsuario){
+            req.session.ID_usuario = loginUsuario.ID_usuario;
+            req.session.nomeUsuario = loginUsuario.nomeUsuario;
+            req.session.emailUsuario = loginUsuario.emailUsuario;
+            req.session.tipoUsuario = loginUsuario.tipoUsuario;
+            if(loginUsuario.tipoUsuario === "Jogador"){
+                const Usuario_jogador = await Jogador.findOne({ where: { ID_usuario: loginUsuario.ID_usuario } });
+                if(Usuario_jogador){
+                    req.session.nicknameJogador = Usuario_jogador.nicknameJogador;
+                    loginUsuario.dataValues.nicknameJogador = Usuario_jogador.nicknameJogador;
+                }
+            };
+
+            const Resposta = new RespostaHTTP(true, "Login de usuário feito com sucesso", null, loginUsuario);
+            Resposta.ExibiMensagem();
+            return res.status(200).json(Resposta.RetornaResposta('returnDado'));
+
+        } else{
+            const Resposta = new RespostaHTTP(true, "Não há usuário cadastrado relacionado ao email e senha fornecidos", null);
+            Resposta.ExibiMensagem('Erro');
+            return res.status(404).json(Resposta.RetornaResposta());
+        }
         
     } catch (error) {
-        const Resposta = new RespostaHTTP(false, "", error.message || error);
+        const Resposta = new RespostaHTTP(false, "Erro no login do usuário ao sistema", error.message || error);
         Resposta.ExibiMensagem('Erro');
         return res.status(500).json(Resposta.RetornaResposta());
     }
@@ -15,9 +57,30 @@ const Login = async (req, res) => {
 
 const ConfirmLogin = async (req, res) => {
     try {
+        const { verificacaoLogado=true } = req.body;
+        
+        const { ID_usuario, emailUsuario, tipoUsuario, nomeUsuario } = req.session;
+        const dadosSessao = ID_usuario && emailUsuario && emailUsuario && nomeUsuario;
+
+        if(verificacaoLogado && dadosSessao){
+            let Usuario_ID = await Usuario.findByPk(ID_usuario, { attributes: ['ID_usuario', 'nomeUsuario', 'emailUsuario', 'tipoUsuario'] });
+            if(Usuario_ID.tipoUsuario === "Jogador"){
+                const Usuario_jogador = await Jogador.findOne({ where: { ID_usuario: ID_usuario }, attributes: ['nicknameJogador'] });
+                Usuario_jogador ? Usuario_ID.dataValues.nicknameJogador = Usuario_jogador.nicknameJogador : null;
+            }
+
+            const Resposta = new RespostaHTTP(true, "Usuário logado no sistema", null, Usuario_ID);
+            Resposta.ExibiMensagem();
+            return res.status(200).json(Resposta.RetornaResposta('returnDado'));
+
+        } else{
+            const Resposta = new RespostaHTTP(false, "Não há usuário logado no sistema", null);
+            Resposta.ExibiMensagem();
+            return res.status(200).json(Resposta.RetornaResposta());
+        }
         
     } catch (error) {
-        const Resposta = new RespostaHTTP(false, "", error.message || error);
+        const Resposta = new RespostaHTTP(false, "Erro na verificação de login do usuário no sistema", error.message || error);
         Resposta.ExibiMensagem('Erro');
         return res.status(500).json(Resposta.RetornaResposta());
     }
@@ -25,9 +88,23 @@ const ConfirmLogin = async (req, res) => {
 
 const Logout = async (req, res) => {
     try {
+        const { logout=true } = req.body;
+
+        req.session.destroy((err) => {
+            if(err) {
+                const Resposta = new RespostaHTTP(false, "Erro no logout do usuário do sistema", err);
+                Resposta.ExibiMensagem('Erro');
+                return res.status(500).json(Resposta.RetornaResposta());
+            }
+
+            res.clearCookie('registerXadrez');
+            const Resposta = new RespostaHTTP(true, "Logout de usuário realizado com sucesso", null);
+            Resposta.ExibiMensagem();
+            return res.status(500).json(Resposta.RetornaResposta());
+        });
         
     } catch (error) {
-        const Resposta = new RespostaHTTP(false, "", error.message || error);
+        const Resposta = new RespostaHTTP(false, "Erro no logout do usuário no sistema", error.message || error);
         Resposta.ExibiMensagem('Erro');
         return res.status(500).json(Resposta.RetornaResposta());
     }
